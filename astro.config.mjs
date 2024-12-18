4a5e0a1c11b4238f80952bff35055ec0d624be21
Juanmanms <juanmanms@gmail.com>
2024-12-18 08:08:28 +0100

    refactor: add sitemap integration and update dependencies in configuration files

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,9 @@ import tailwind from '@astrojs/tailwind';
 
 import react from '@astrojs/react';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: 'https://pequenido.es',
+  integrations: [tailwind(), react(), sitemap()]
 });
